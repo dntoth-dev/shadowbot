cc032f2c37e63c7592e317cbe85e7afeb1d490c7
dntoth-dev <toth.daniel@e5vos.hu>
2025-12-20 00:03:49 +0100

    initial commands
    final step before placing bot on 24/7 availability on KataBump

--- main.py
+++ main.py
@@ -4,7 +4,34 @@ import os
 import discord
 
 TOKEN = os.getenv('BOT_TOKEN')
+DEV = os.getenv('DEV_NAME')
+GUILD_OWNER = os.getenv('OWNER_NAME')
 
 intents = discord.Intents.default()
 intents.message_content = True
 client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'Logged in as {client.user}.')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith('!devtest'):
+        if message.author.name == DEV:
+            await message.channel.send(f'devtest. Hello world! \n`Client connected to Discord Gateway & logged in as {client.user}.`')
+        else:
+            await message.channel.send('You do not have permission to use this command.')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith("!am-i-shadow"):
+        if message.author.name == GUILD_OWNER:
+            await message.channel.send(f"Yes, you are {GUILD_OWNER}.")
+        else:
+            await message.channel.send(f"You ain't {GUILD_OWNER} :(")
+client.run(TOKEN)
